95ba240f997fe672d9839d356e27487f9e552916
Kirill Radzikhovskyy <kirillrdy@gmail.com>
2018-09-16 11:04:02 +1000

    Initial version that just lists datasets

--- go.mod
+++ go.mod
@@ -1,1 +1,6 @@
 module github.com/kirillrdy/zfs-web
+
+require (
+	github.com/dustin/go-humanize v0.0.0-20180713052910-9f541cc9db5d
+	github.com/mistifyio/go-zfs v2.1.1+incompatible
+)
--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/dustin/go-humanize"
+	"github.com/mistifyio/go-zfs"
+	"log"
+)
+
+func crash(err error) {
+	if err != nil {
+		log.Panic(err)
+	}
+}
 
 func main() {
-	fmt.Println("vim-go")
+	datasets, err := zfs.Filesystems("")
+	crash(err)
+	for _, dataset := range datasets {
+		fmt.Printf("%#v %v %v\n", dataset.Name, humanize.Bytes(dataset.Used), humanize.Bytes(dataset.Avail))
+	}
+
 }
