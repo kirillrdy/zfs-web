98aa99ff74295261b13e7c87d9f78ee1884195b3
Kirill Radzikhovskyy <kirillrdy@gmail.com>
2019-11-09 15:24:16 +1100

    Moved font fetching to index.html

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -41,7 +41,6 @@ function App() {
 
     return (
       <div>
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
         <Grid container spacing={10}>
           <Grid item>
               <ListItem button>
